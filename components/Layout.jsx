e8739e50e492c155ce825ca5d4627ba5846f806e
AndreiMorosan <morosan_andrei20@yahoo.com>
2022-05-10 21:35:24 +0300

    a noua

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 
-import Navbar from './Navbar';
+import Navbar from './Navbar.jsx';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
